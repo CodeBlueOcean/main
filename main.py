c318f16043887e1d6adf6726ec748d8618137f8d
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-04 20:14:59 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+# another example
+is_old = False
+is_licenced = True
+
+if is_old:
+    print('you are old enough to drive!')
+else:
+    print('checheck')
+
+# another example
+is_old = True
+is_licenced = True
+
+if is_old:
+    print('you are old enough to drive!')
+else:
+    print('checheck')
+
+# another example
+is_old = True
+is_licenced = True
+
+if is_old:
+    print('you are old enough to drive!')
+else:
+    print('checheck')
+
+print('okoko')
+
+
+# another example
+is_old = True
+is_licenced = True
+
+if is_old:
+    print('you are old enough to drive!')
+elif is_licenced:
+    print('you can drive now!')
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+# another example
+is_old = False
+is_licenced = True
+
+if is_old:
+    print('you are old enough to drive!')
+elif is_licenced:
+    print('you can drive now!')
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+# another example
+is_old = False
+is_licenced = False
+
+if is_old:
+    print('you are old enough to drive!')
+elif is_licenced:
+    print('you can drive now!')
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+# another example
+is_old = False
+is_licenced = False
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+# another example
+is_old = False
+is_licenced = False
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+# elif condition: 
+# elif condition: 
+else:
+    print('you are not of age!')
+
+print('okoko')
+
+
